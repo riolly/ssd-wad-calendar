db0fb6762a532aa217e645c2f07f91889eb0ac09
riolly <alberto.riolly@gmail.com>
2023-07-29 09:41:12 +0700

    github link & h1 5xl

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useDatedStore } from "./lib/store";
 import DateCell, { PrevNextDateCell } from "./components/DateCell";
 import ScheduleCreateDialog from "./components/ScheduleCreate";
 import ScheduleEditDialog from "./components/ScheduleEdit";
+import { GithubIcon } from "lucide-react";
 
 export default function HomePage() {
   // toDate mean today
@@ -18,10 +19,17 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b overflow-clip from-blue-950 to-violet-950 text-white relative dark">
       <div className="absolute w-[200%] h-[200%] -top-[37%] bg-pattern bg-opacity-50 rotate-12"></div>
       <div className="absolute w-full h-full bg-gradient-to-r from-slate-950/80 via-transparent to-slate-950/80"></div>
-      <div className="absolute w-full h-1/3 bg-gradient-to-b from-slate-950 to-transparent"></div>
+      <div className="absolute w-full h-1/3 bg-gradient-to-b from-slate-950 to-transparent">
+      </div>
+      <div className="fixed top-0 right-0 p-6">
+        <a href="https://github.com/riolly/ssd-wad-calendar" target="_blank">
+          <GithubIcon size={48} className="text-black rounded-full bg-gray-200 p-1.5" />
+        </a>
+      </div>
+
 
       <div className="container flex flex-col items-center py-12 ">
-        <h1 className="-mt-2 mb-8 text-4xl font-semibold z-10">
+        <h1 className="-mt-2 mb-6 text-5xl font-semibold z-10">
           {toDated.monthStr} {toDated.year}
         </h1>
 
