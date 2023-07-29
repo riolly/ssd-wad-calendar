498c4760fddc6db0bf32596e148a4cfcacb748e7
riolly <alberto.riolly@gmail.com>
2023-07-29 09:18:23 +0700

    bg image

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,17 @@ export default function HomePage() {
   const dateds = useDatedStore((state) => state.dateds);
 
   return (
-    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-blue-950 to-slate-900 text-white">
-      <div className="container flex flex-col items-center py-12">
-        <h1 className="-mt-2 mb-8 text-4xl font-semibold">
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b overflow-clip from-blue-950 to-violet-950 text-white relative">
+      <div className="absolute w-[200%] h-[200%] -top-[37%] bg-pattern bg-opacity-50 rotate-12"></div>
+      <div className="absolute w-full h-full bg-gradient-to-r from-slate-950/80 via-transparent to-slate-950/80"></div>
+      <div className="absolute w-full h-1/3 bg-gradient-to-b from-slate-950 to-transparent"></div>
+
+      <div className="container flex flex-col items-center py-12 ">
+        <h1 className="-mt-2 mb-8 text-4xl font-semibold z-10">
           {toDated.monthStr} {toDated.year}
         </h1>
 
-        <div className="grid grid-cols-7 overflow-clip rounded-t-xl">
+        <div className="grid grid-cols-7 overflow-clip rounded-t-xl backdrop-blur-lg shadow-xl shadow-white/10">
           {DAYS.map((day) => (
             <DayHeading key={day} day={day} />
           ))}
@@ -50,7 +54,7 @@ export default function HomePage() {
 
 function DayHeading(props: { day: string }) {
   return (
-    <div className="flex justify-center bg-blue-100/75 py-2 text-lg font-semibold text-slate-800">
+    <div className="flex justify-center bg-blue-100 py-2 text-lg font-semibold text-blue-950">
       {props.day}
     </div>
   );
